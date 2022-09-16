f3f29b585f7744fcdd6048273fd229a1df28d45c
Nahuel Benitez <nahuelbenitezs2002@gmail.com>
2022-09-16 09:09:53 -0300

    slider

--- js/contablito.js
+++ js/contablito.js
@@ -1,55 +1,18 @@
-const subtotal = document.getElementById("subtotal");
-const descipcion = document.getElementById("Descipcion");
-const compra = document.getElementById("Compra");
-const venta = document.getElementById("Venta");
-const basico = document.getElementById("Basico");
-const minimo = document.getElementById("Minimo");
-const exento = document.getElementById("Exento");
-const totalVentas = document.getElementById("total-ventas");
-const totalCompras = document.getElementById("total-compras");
-const table = document.getElementById("table");
-const boton = document.getElementById("ingBtn");
+const buttons = document.querySelectorAll("[data-carousel-button]")
 
-let totalVenta = 0;
-let totalCompra = 0;
+buttons.forEach(button => {
+  button.addEventListener("click", () => {
+    const offset = button.dataset.carouselButton === "next" ? 1 : -1
+    const slides = button
+      .closest("[data-carousel]")
+      .querySelector("[data-slides]")
 
-boton.addEventListener("click", () => {
-  let descipcionValue = descipcion.value;
-  let subtotalValue = subtotal.value;
+    const activeSlide = slides.querySelector("[data-active]")
+    let newIndex = [...slides.children].indexOf(activeSlide) + offset
+    if (newIndex < 0) newIndex = slides.children.length - 1
+    if (newIndex >= slides.children.length) newIndex = 0
 
-  let tipotransaccion = "";
-
-  let ivaValue = 0;
-  let ivaBasicoValue = subtotalValue * basico.value;
-  let ivaMinimoValue = subtotalValue * minimo.value;
-
-  if (
-    descipcionValue &&
-    subtotalValue &&
-    (compra.checked || venta.checked) &&
-    (basico.checked || minimo.checked || exento.checked)
-  ) {
-    if (compra.checked) {
-      tipotransaccion = "Compra";
-      totalCompra += parseInt(subtotalValue);
-    } else {
-      tipotransaccion = "Venta";
-      totalVenta += parseInt(subtotalValue);
-    }
-
-    if (basico.checked) {
-      ivaValue = ivaBasicoValue;
-    } else if (minimo.checked) {
-      ivaValue = ivaMinimoValue;
-    } else {
-      ivaValue = exento.value;
-    }
-
-    let totalIva = parseInt(subtotalValue) + parseInt(ivaValue);
-
-    table.innerHTML += `<tr><td>${descipcionValue}</td><td>${tipotransaccion}</td><td>${subtotalValue}</td><td>${ivaValue}</td><td>${totalIva}</td></tr>`;
-  }
-
-  totalVentas.innerHTML = `$${totalVenta}`;
-  totalCompra.innerHTML = `$${totalCompra}`;
-});
+    slides.children[newIndex].dataset.active = true
+    delete activeSlide.dataset.active
+  })
+})
